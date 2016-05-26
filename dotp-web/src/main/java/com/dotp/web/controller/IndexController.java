a490728505fc896a55a20adfc1d51db645d842ad
liwenhe <411934049@qq.com>
2016-05-26 13:02:16 +0800

    集成velocity

--- dotp-web/src/main/java/com/dotp/web/controller/IndexController.java
+++ dotp-web/src/main/java/com/dotp/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.dotp.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by sherw on 2016/5/25.
@@ -11,9 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IndexController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
-        return "hello world";
+        return "/index";
     }
 
 }
